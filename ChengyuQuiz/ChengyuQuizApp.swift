8df77f0eaf26747a690ccf071098da5e4cd2e6d9
Ed Allonby <edward.allonby@gmail.com>
2022-05-23 21:48:19 +0100

    Initial Commit

---
+++ ChengyuQuiz/ChengyuQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChengyuQuizApp.swift
+//  ChengyuQuiz
+//
+//  Created by Ed Allonby on 23/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct ChengyuQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
